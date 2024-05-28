e5c58704e10c7821addcf665181d19fc3ee066bb
Lucca-Urso <lgursog13@gmail.com>
2024-05-28 16:30:19 -0300

    change main to names

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("testing again");
-        System.out.println("Changing main ");
+        System.out.println("Nome: Lucca, Fernando e Henrique");
     }
 }

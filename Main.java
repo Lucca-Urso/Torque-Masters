fc4d5116ffd35889935ab125fae2d8a74b4c1fb0
Lucca-Urso <lgursog13@gmail.com>
2024-05-28 16:31:06 -0300

    name calvetti

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nome: Lucca, Fernando e Henrique");
+        System.out.println("Nome: Calvetti");
     }
 }

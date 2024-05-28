55b3cc1bc27771aa68917ecba234c9146f66de77
Lucca-Urso <lgursog13@gmail.com>
2024-05-28 16:31:49 -0300

    ad names section

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nome: Calvetti");
+        System.out.println("Nome: Lucca");
+        System.out.println("Nome: ");
+        System.out.println("Nome: ");
     }
 }

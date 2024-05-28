3b4b0eb591d24ea9cab8874f4627725da7497e19
M3ngal <fernando.magimenez@gmail.com>
2024-05-28 16:39:40 -0300

    Teste nome

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Nome: Lucca");
-        System.out.println("Nome: ");
+        System.out.println("Nome: Fernando");
         System.out.println("Nome: ");
     }
 }

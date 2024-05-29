41feee9a22671d78ba050e901789529342c565f7
Lucca-Urso <lgursog13@gmail.com>
2024-05-29 10:36:38 -0300

    continuacao da criacao da classe do motor

--- Car.java
+++ Car.java
@@ -23,53 +23,116 @@ public class Car {
         private String traction;
 
         Engine(String engineType, double cylinders, String aspiration, String fuel, String engineMaterial, String traction) {
-            //Coleta e validação das caracteristicas do tipo do motor
+            //Coleta dos valores dos atributos do tipo do motor
+            //tipo do motor
             switch (engineType) {
-                case "Inline":
-                    System.out.println("Enter cylinder ammount:");
+                case "Em linha":
+                    System.out.println("Enter engine cylinder number:");
                     cylinderNumber = sc.nextInt();
-
-                    if (cylinderNumber != 3 && cylinderNumber != 4 && cylinderNumber != 5)
-                        System.out.println("Incorrect value!");
 
                     break;
                     
 
                 case "Boxer":
-                    System.out.println("Enter cylinder ammount:");
+                    System.out.println("Enter engine cylinder number:");
                     cylinderNumber = sc.nextInt();
-
-                    if (cylinderNumber != 4 && cylinderNumber != 6)
-                        System.out.println("Incorrect value!");
                     
                     break;
 
                 case "V":
-                    System.out.println("Enter cylinder ammount:");
+                    System.out.println("Enter engine cylinder number:");
                     cylinderNumber = sc.nextInt();
-
-                    if (cylinderNumber != 6 && cylinderNumber != 8 && cylinderNumber != 10 && cylinderNumber != 12)
-                        System.out.println("Incorrect value!");
 
                     break;
             
                 default:
-                    System.out.println("Incorrect value!");
+                    break;
             }
 
-            if (cylinders != 1.0 && cylinders != 1.6 && cylinders != 2.0 && cylinders != 2.4 && cylinders != 3.0 && cylinders != 3.6 && cylinders != 4.2)
-                System.out.println("Incorrect value!"); 
+            //aspiração
+            switch (aspiration) {
+                case "aspirado naturalmente":
+                    
+                    break;
+
+                case "turbo compressor":
+                    
+                    break;
+
+                case "super compressor":
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+
+            //tipo do combustivel
+            switch (fuel) {
+                case "gasolina":
+                    
+                    break;
+
+                case "diesel":
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+
+            //material do motor
+            switch (engineMaterial) {
+                case "ferro fundido":
+                    
+                    break;
+
+                case "liga de aluminio":
+                    
+                    break;
+
+                case "liga de titanio":
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+
+            //tipo de tração
+            switch (traction) {
+                case "traseira":
+                    
+                    break;
+            
+                case "dianteira":
+                    
+                    break;
+                
+                case "integral":
+                    
+                    break;
+
+                default:
+                    break;
+            }
+
+            
+
 
 
             //passagem de valores para os atributos do construtores
             this.engineType = engineType;
             this.cylinders = cylinders;
-            
+            this.aspiration = aspiration;
+            this.fuel = fuel;
+            this.engineMaterial = engineMaterial;
+            this.traction = traction;
+        }
 
-
-
-
-
+        public String toString() {
+            return String.format("-------------------Car-------------------\n1 - Engine:\nType: %s; Cylinders: %f; Aspiration: %s; Fuel: %s; Material: %s" + 
+            "; Traction: %s", engineType, cylinders, aspiration, fuel, engineMaterial, traction);
         }
     }
 
@@ -114,30 +177,52 @@ public class Car {
     }
 
     //Construtor do carro, ja aplica todas as fórmulas de acordo com os componentes selecionados
-    Car (String engineType, double cylinders, String aspiration, String fuel, String engineMaterial, String traction, 
-    double cost, double consumption, double weight, double maxSpeed, double acceleration, 
-    double handling, double brakesPower) {
-        Engine carEngine = new Engine(engineType, 2.0, "Super Compressor", "Gas", "Titanium", "Rear");
+    Car (String engineType, double cylinders, String aspiration, String fuel, String engineMaterial, String traction) {
+        Engine carEngine = new Engine(engineType, cylinders, aspiration, fuel, engineMaterial, traction);
         Brakes carBrakes = new Brakes();
         Tires carTires = new Tires();
         Chassis carChassis = new Chassis();
         Suspension carSuspension = new Suspension();
         BodyPaint carBodyPaint = new BodyPaint();
+
+        System.out.println(carEngine.toString());
     }
-
-
 
     //Método main para teste das classes inicialmente, posteriormente realizar testes através de interfaces gráficas
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        System.out.println("--------------------Engine Informations--------------------\n");
         System.out.println("Enter engine type: ");
         String engineType = sc.nextLine();
         System.out.println("Enter cylinders: ");
-        Double cylinders = sc.nextDouble();
+        Double cylinders = Double.parseDouble(sc.nextLine());
+        System.out.println("Enter engine aspiration type: ");
+        String aspiration = sc.nextLine();
+        System.out.println("Enter fuel type: ");
+        String fuel = sc.nextLine();
+        System.out.println("Enter engine material: ");
+        String engineMaterial = sc.nextLine();
+        System.out.println("Enter traction type: ");
+        String traction = sc.nextLine();
 
-        Car subaru = new Car(engineType, cylinders);
+        System.out.println("\n--------------------Car Components--------------------\n");
+        System.out.println("Enter car brake type: ");
+        String brakes = sc.nextLine();
+        System.out.println("Enter car tires type: ");
+        String tires = sc.nextLine();
+        System.out.println("Enter car chassis type: ");
+        String chassis = sc.nextLine();
+        System.out.println("Enter car suspension type: ");
+        String suspension = sc.nextLine();
+        System.out.println("Enter car chassis color: ");
+        String color = sc.nextLine();
 
+        Car subaru = new Car(engineType, cylinders, aspiration, fuel, engineMaterial, traction);
+        
+
+
+        
         
         
     }
